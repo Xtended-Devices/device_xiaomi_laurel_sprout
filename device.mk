38d4292d99c71752f22c93b92c5d9f37e5d47882
oddlyspaced <oddlyspaced1@gmail.com>
2020-04-26 04:27:46 +0000

    Fix fod

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ PRODUCT_TARGET_VNDK_VERSION := 29
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    vendor/aosp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml \
     $(LOCAL_PATH)/permissions/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 # A/B
@@ -127,7 +126,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_trinket
+    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_trinket
 
 # HIDL
 PRODUCT_PACKAGES += \
