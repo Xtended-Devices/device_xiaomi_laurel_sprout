5e9cec116b26f6e817913ddb479e0fb5eb07ae85
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-06 15:43:32 +0530

    Rename bootleg to evox

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Bootleggers stuff.
+# Inherit some common Evolution-X stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
